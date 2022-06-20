4991899a4c3ea4533282e9f5ced9dabd3bffc3ed
SalmaMansourigit config --global  user.email <mansourisalma038@gmail.com>
2022-06-21 00:47:26 +0100

    proj5

--- spiders/ninewest.py
+++ spiders/ninewest.py
@@ -1,15 +1,37 @@
 import requests
+import json
+from utils import get_ld_json
 
-from utils import get_ld_json, parse_json
 
+class Ninewest:
 
-class NineWest:
+    @staticmethod
+    def parse_json(ld: json):
+        return {
+            'sku': ld['sku'],
+            'title': ld['name'],
+            'description': ld['description'],
+            'price': ld['offers']['price'],
+            'currency': ld['offers']['priceCurrency'],
+            'price'  : ld['offers']['price'],
+            'availability': ld['offers']['availability'],
+            'url': ld['offers']['url'],
+            'priceValidUntil': ld['offers']['priceValidUntil'],
+            'mpn': ld['mpn'],
+            'brand': ld['brand'],
+            'image': ld['image'],
+            "seller_name": ld['offers']['seller']['name'],
+        }
 
-    def __init__(self, product_url):
-        self.product_url = product_url
-
-    def product_info(self):
-        response = requests.get(self.product_url)
+    @classmethod
+    def product_info(cls, product_url):
+        response = requests.get(product_url)
         ld_json = get_ld_json(response)
-        data = parse_json(ld_json)
+        data = cls.parse_json(ld_json)
+        data['product_url'] = product_url
         return data
+
+
+p = Ninewest()
+info = p.product_info('https://ninewest.com/collections/currently-coveting/products/polka-slingback-heeled-sandals-in-clear')
+print(info)
