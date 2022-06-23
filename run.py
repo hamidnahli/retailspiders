97f75534a96289c9f4619ac6dc8c796ce0911e49
Lamrabate_Imran <i.lamrabate@gmail.com>
2022-06-24 00:56:24 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
+from spiders.anthropologie import Anthropologie
 from spiders.ninewest import NineWest
 
 if __name__ == '__main__':
-    url = 'https://ninewest.com/products/speakup-almond-toe-flats-in-black-floral'
-    p = NineWest(product_url=url)
+    #url = 'https://ninewest.com/products/speakup-almond-toe-flats-in-black-floral'
+    #p = NineWest(product_url=url)
+    #print(p.get_product_info())
+    url = 'https://www.anthropologie.com/shop/facial-rounds?color=000&recommendation=home-hpg-tray-1-sfrectray-homepagetrendingmostpurchased&type=STANDARD&size=One%20Size&quantity=1'
+    p = Anthropologie(product_url=url)
     print(p.get_product_info())
