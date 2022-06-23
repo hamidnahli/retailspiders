818e42a23f95dc9efe0fbc8ea1f3b202c419e4f9
Lamrabate_Imran <i.lamrabate@gmail.com>
2022-06-24 00:56:21 +0100

    Update utils.py

--- items/utils.py
+++ items/utils.py
@@ -96,3 +96,15 @@ def parse_stamped_reviews(rid, rtype, product_name, product_sku):
                 reviews.append(review)
         page += 1
     return rating, count, reviews
+
+def global_headers():
+    return {
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9,ar;q=0.8,fr;q=0.7,de;q=0.6',
+    }
