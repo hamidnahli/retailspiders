d747fc2b86b7455ccfcd0e227029f46e4730ef6d
SalmaMansourigit config --global  user.email <mansourisalma038@gmail.com>
2022-07-02 16:08:44 +0100

    'task_done'

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
-from spiders.ninewest import NineWest
+from spiders.clinique import Clinique
+
+
 
 if __name__ == '__main__':
-    url = 'https://ninewest.com/products/speakup-almond-toe-flats-in-black-floral'
-    p = NineWest(product_url=url)
+    
+    url = 'https://www.clinique.com/product/4034/87057/skincare/serum/clinique-smart-clinical-repairtm-wrinkle-correcting-serum?size=1.7_oz._%2F_50_ml'
+    p =Clinique(product_url=url)
     print(p.get_product_info())
+    print(p.get_product_review())
